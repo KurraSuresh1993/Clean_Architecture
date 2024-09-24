8238c1059940d8078c35619c9897b03223cca7c2
saresh <karrasaresh@gmail.com>
2024-09-24 07:06:29 +0000

    initial solution is added

---
+++ CleanArchitecture/MyApp.Infrastruture/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace MyApp.Infrastruture
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
